5a178518cbe2a9c1c1f13ee0877d33265324e702
sanjayr-12 <sanjayr.bit22@gmail.com>
2025-03-29 22:29:57 +0530

    add zustand dependency and update login route path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
