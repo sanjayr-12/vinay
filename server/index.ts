4cc5ab33bf197dfc5ce57e17b0ed0ba60c47df27
sanjayr-12 <sanjayr.bit22@gmail.com>
2025-04-06 15:08:48 +0530

    update .gitignore files; add static file serving in server index; enhance build and start scripts

--- server/index.ts
+++ server/index.ts
@@ -6,12 +6,18 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import configCloudinary from "./config/cloudinary";
 import userRouter from "./routes/user.routes";
+import path from "path";
 
 const app = express();
 app.use(cors({ origin: process.env.ORIGIN }));
 dotenv.config();
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "../../client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/dist", "index.html"));
+});
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
