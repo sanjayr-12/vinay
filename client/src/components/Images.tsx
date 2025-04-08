be4d71c85c4edf3c3d3410d8747e40ff9980a018
sanjayr-12 <sanjayr.bit22@gmail.com>
2025-04-08 11:22:02 +0530

    add modal functionality to Images component for image preview

--- client/src/components/Images.tsx
+++ client/src/components/Images.tsx
@@ -10,9 +10,17 @@ const Images = () => {
   return (
     <>
       {images.map((i: ImageType) => (
-        <div className="card bg-base-100 w-96 shadow-xl" key={i.url}>
+        <div className="card bg-base-100 w-96 h-96 shadow-xl" key={i.url}>
           <figure className="w-full h-full">
-            <img src={i.url} alt={i.imageName} className="object-cover w-full h-full" />
+            <img
+              src={i.url}
+              alt={i.imageName}
+              className="object-cover w-full h-full cursor-pointer"
+              onClick={() => {
+                const modal = document.getElementById(`modal-${i.url}`) as HTMLDialogElement;
+                modal?.showModal();
+              }}
+            />
           </figure>
           <div className="card-body">
             <h2 className="card-title">{i.imageName}</h2>
@@ -29,6 +37,17 @@ const Images = () => {
               <button className="btn btn-secondary text-lg">♡</button>
             </div>
           )}
+          <dialog id={`modal-${i.url}`} className="modal">
+            <div className="modal-box">
+              <img src={i.url} alt={i.imageName} className="object-cover w-full h-full" />
+              <div className="modal-action">
+                <button className="btn" onClick={() => {
+                  const modal = document.getElementById(`modal-${i.url}`) as HTMLDialogElement;
+                  modal?.close();
+                }}>Close</button>
+              </div>
+            </div>
+          </dialog>
         </div>
       ))}
     </>
