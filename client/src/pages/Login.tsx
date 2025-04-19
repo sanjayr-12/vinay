2405fc5c30bcbbdb3206db46d8b25ceb58b64679
sanjayr-12 <sanjayr.bit22@gmail.com>
2025-04-19 21:52:02 +0530

    fix: add spacing paragraph in Login component

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -6,6 +6,7 @@ const Login = () => {
       <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 className="text-5xl font-bold">Specview</h1>
+          <p className="py-6"></p>
           <div className="flex justify-center">
             <Oauth />
           </div>
