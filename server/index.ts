bf1cad35549493cdd59c2ffd1778280e8d787e30
sanjayr-12 <sanjayr.bit22@gmail.com>
2025-04-20 15:28:07 +0530

    fix: update static file paths for client distribution

--- server/index.ts
+++ server/index.ts
@@ -31,14 +31,14 @@ if (cluster.isPrimary) {
 
   app.use(express.json({ limit: "10mb" }));
   app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, "../client/dist")));
+  app.use(express.static(path.join(__dirname, "../../client/dist")));
 
   app.use("/api/auth", authRouter);
   app.use("/api/user", userRouter);
   app.use("/api/utils", utilRouter);
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../../client/dist", "index.html"));
   });
 
   app.listen(process.env.PORT, async () => {
