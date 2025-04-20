7c62b88accf4d148527d9a21dfcefff63b753fcb
sanjayr-12 <sanjayr.bit22@gmail.com>
2025-04-20 15:23:16 +0530

    feat: implement clustering for improved server performance

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,5 @@
+import cluster from "cluster";
+import os from "os";
 import express from "express";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.routes";
@@ -10,24 +12,41 @@ import path from "path";
 import utilRouter from "./utils/utils.routes";
 import { reStart } from "./utils/self.cron";
 
-const app = express();
-// app.use(cors({ origin: process.env.ORIGIN }));
-dotenv.config();
-app.use(express.json({ limit: "10mb" }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "../../client/dist")));
+const numCPUs = os.cpus().length;
 
-app.use("/api/auth", authRouter);
-app.use("/api/user", userRouter);
-app.use("/api/utils", utilRouter);
+if (cluster.isPrimary) {
+  console.log(`Master ${process.pid} is running`);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../client/dist", "index.html"));
-});
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-app.listen(process.env.PORT, async () => {
-  await connectDB();
-  await configCloudinary();
-  reStart();
-  console.log("server started and db connected");
-});
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died. Spawning a new one...`);
+    cluster.fork();
+  });
+} else {
+  dotenv.config();
+  const app = express();
+
+  app.use(express.json({ limit: "10mb" }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.use("/api/auth", authRouter);
+  app.use("/api/user", userRouter);
+  app.use("/api/utils", utilRouter);
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+  });
+
+  app.listen(process.env.PORT, async () => {
+    await connectDB();
+    await configCloudinary();
+    reStart();
+    console.log(
+      `Worker ${process.pid} started and server running on port ${process.env.PORT}`
+    );
+  });
+}
