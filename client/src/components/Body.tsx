0fcc101cae666594660c17fca0d3fa8957bb6605
sanjayr-12 <sanjayr.bit22@gmail.com>
2025-04-21 17:24:42 +0530

    fix: image load on home page

--- client/src/components/Body.tsx
+++ client/src/components/Body.tsx
@@ -1,19 +1,6 @@
-import { useEffect } from "react";
-import { getImages } from "../apis/apiStore";
-import { useImageStore } from "../store/Store";
 import HomeIntro from "./HomeIntro";
 
 const Body = () => {
-  const setImage = useImageStore((state) => state.setImages);
-  const render = useImageStore((state) => state.render);
-
-  useEffect(() => {
-    (async () => {
-      const result = await getImages();
-      setImage(result.images);
-    })();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [render]);
   return (
     <div className="mt-5 flex justify-center items-center flex-wrap gap-10">
       <HomeIntro />
